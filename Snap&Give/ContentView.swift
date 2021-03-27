9b332cb6f38c647215a9de75ad3caeaa26b37e0f
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-03-27 09:41:50 -0700

    Initial Commit

---
+++ Snap&Give/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snap&Give
+//
+//  Created by Shengyuan Lu on 3/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Snap&Give/Snap_GiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snap_GiveApp.swift
+//  Snap&Give
+//
+//  Created by Shengyuan Lu on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Snap_GiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
