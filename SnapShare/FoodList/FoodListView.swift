13b84af5b59211ac4b2de2550585f7d5a25fee06
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-03-28 04:35:23 -0700

    Launch screen

--- SnapShare/FoodList/FoodListView.swift
+++ SnapShare/FoodList/FoodListView.swift
@@ -8,58 +8,62 @@ struct FoodListView: View {
         
         NavigationView {
             
-            if foodList.foodDict.count == 0 {
-                Text("NO Food Available for Donate.")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.secondary)
-                    .padding()
-            }
-            
-            List {
-                ForEach(Array(foodList.foodDict.keys), id: \.self) { key in
-                    FoodListCell(image: foodList.foodDict[key]!, name: key)
-                        .padding(5)
+            VStack {
+                if foodList.foodDict.count == 0 {
+                    Text("NO Food Available for Donate.")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.secondary)
+                        .padding()
                 }
-                .onDelete(perform: { indexSet in
-                    
-                    var arr = [String]()
-                    
-                    for index in indexSet {
-                        arr.append(Array(foodList.foodDict.keys)[index])
+                
+                List {
+                    ForEach(Array(foodList.foodDict.keys), id: \.self) { key in
+                        FoodListCell(image: foodList.foodDict[key]!, name: key)
+                            .padding(5)
                     }
+                    .onDelete(perform: { indexSet in
+                        
+                        var arr = [String]()
+                        
+                        for index in indexSet {
+                            arr.append(Array(foodList.foodDict.keys)[index])
+                        }
+                        
+                        for key in arr {
+                            foodList.foodDict.removeValue(forKey: key)
+                        }
+                        
+                    })
+                }
+                
+                VStack(spacing: 10) {
                     
-                    for key in arr {
-                        foodList.foodDict.removeValue(forKey: key)
-                    }
-                    
-                })
+                    // Continue
+                    Button(action: {
+                        
+                        
+                        
+                    }, label: {
+                        Text("Find me a Food Bank to Donate 🥦")
+                            .bold()
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.1)
+                            .frame(width: UIScreen.main.bounds.width/1.5, alignment: .center)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(8)
+                        
+                    })
+                }
+                .padding(.top, 30)
+                .padding(.bottom, 20)
+                .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0.0, y: 0.0)
             }
             .navigationTitle("My Donation List")
             .navigationBarTitleDisplayMode(.inline)
-            
-            VStack(spacing: 10) {
-                
-                // Continue
-                Button(action: {
-                    
-                    
-                }, label: {
-                    Text("Find me a Food Bank to Donate! ☺️")
-                        .bold()
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.1)
-                        .frame(width: UIScreen.main.bounds.width/1.5, alignment: .center)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.green)
-                        .cornerRadius(8)
-                    
-                })
-            }
-            .padding(.top, 30)
-            .padding(.bottom, 5)
-            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0.0, y: 0.0)
+
         }
     }
 }
