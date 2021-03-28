7b78e4f6614bc8ab6c4d1d7d471cb0a99c1aebbe
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-03-28 04:46:20 -0700

    Commit

---
+++ SnapShare/DonateView/FoodBankCell.swift
@@ -0,0 +1,20 @@
+//
+//  FoodBankCell.swift
+//  SnapShare
+//
+//  Created by Shengyuan Lu on 3/28/21.
+//
+
+import SwiftUI
+
+struct FoodBankCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FoodBankCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodBankCell()
+    }
+}
